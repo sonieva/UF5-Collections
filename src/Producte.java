1da0f06b3ab489f4a15a729baff699835530f57a
Santi Onieva <s.onieva@sapalomera.cat>
2024-04-12 12:46:15 +0200

    Canvis classe 'Producte'

--- src/Producte.java
+++ src/Producte.java
@@ -1,2 +1,31 @@
-public class Producte {
+public abstract class Producte {
+    private double preu;
+    private String nom;
+    private String codiBarres;
+
+    public Producte(double preu, String nom, String codiBarres) {
+        this.preu = preu;
+        this.nom = nom;
+        this.codiBarres = codiBarres;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
+        this.preu = preu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCodiBarres() {
+        return codiBarres;
+    }
 }
