d878f83e178826f0e3386e79e6bc9bc4d99f6f58
Santi Onieva <s.onieva@sapalomera.cat>
2024-04-16 13:33:18 +0200

    Creacio classe 'Compra'

---
+++ src/Compra.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Compra {
+    public List<Alimentacio> llistaAliments;
+    public List<Textil> llistaTextils;
+    public List<Electronica> llistaElectronics;
+
+    public Compra() {
+        this.llistaAliments = new ArrayList<>();
+        this.llistaTextils = new ArrayList<>();
+        this.llistaElectronics = new ArrayList<>();
+    }
+}
