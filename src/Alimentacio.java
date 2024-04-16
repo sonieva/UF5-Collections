650c424d32c7642e2e39ebc98ba57b7e85ea7db4
Santi Onieva <s.onieva@sapalomera.cat>
2024-04-16 12:22:50 +0200

    Classe 'Alimentacio' acabada

--- src/Alimentacio.java
+++ src/Alimentacio.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Alimentacio extends Producte {
@@ -14,5 +15,6 @@ public class Alimentacio extends Producte {
 
     @Override
     public double getPreu() {
+        return super.getPreu() - super.getPreu() * ((double) 1 / (Duration.between(this.getDataCaducitat(), LocalDateTime.now()).toDays() + 1)) + (super.getPreu() * 0.1);
     }
 }
