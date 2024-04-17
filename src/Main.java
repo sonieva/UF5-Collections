6081eec7b0caee1e63bb40acbf52ff89f8a07125
Santi Onieva <s.onieva@sapalomera.cat>
2024-04-17 17:40:55 +0200

    Creacio classe 'Main'

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int op,op2;
+
+        Scanner sc = new Scanner(System.in);
+        Compra compra = new Compra();
+
+        System.out.println("BENVINGUT AL SAPAMERCAT");
+        System.out.println("-".repeat(15));
+        System.out.println("-".repeat(4) + " INICI " + "-".repeat(4));
+        System.out.println("-".repeat(15));
+        System.out.println("1. Introduir producte");
+        System.out.println("2. Passar per caixa");
+        System.out.println("3. Mostrar carret de compra");
+        System.out.println("0. Sortir");
+
+        do {
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    System.out.println("-".repeat(15));
+                    System.out.println("-".repeat(2) + " PRODUCTE " + "-".repeat(3));
+                    System.out.println("-".repeat(15));
+                    System.out.println("1. Alimentació");
+                    System.out.println("2. Tèxtil");
+                    System.out.println("3. Electrònica");
+                    System.out.println("0. Sortir");
+                    do {
+                        op2 = sc.nextInt();
+                        switch (op2) {
+                            case 1:
+                                try {
+                                    compra.afegirProducte("Alimentacio");
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                } break;
+                            case 2:
+                                try {
+                                    compra.afegirProducte("Textil");
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                } break;
+                            case 3:
+                                try {
+                                    compra.afegirProducte("Electronica");
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                } break;
+                            default: break;
+                        }
+                    } while (op2 != 0);
+                    break;
+                case 2:
+                    compra.passarPerCaixa();
+                    break;
+                case 3:
+                    compra.mostarCarret();
+                    break;
+            }
+        } while (op != 0);
+    }
+}
